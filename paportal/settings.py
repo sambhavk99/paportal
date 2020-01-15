f88025cc0eb8d5a00dcf2910b006bd754f64b8aa
sambhav kanishka <sambhav.kanishka@gmail.com>
2020-01-16 00:26:39 +0530

    Enabled Django Channels

--- paportal/settings.py
+++ paportal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'paportal.wsgi.application'
+ASGI_APPLICATION = 'fsportal.routing.application'
 
 
 # Database
